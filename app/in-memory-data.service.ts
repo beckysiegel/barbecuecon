ea9ba2bf49754bf773eb1fa2fbafb55e6d3cf4f2
Ben Actis <bactis@synack.com>
2016-08-16 12:02:35 -0700

    Added content to schedule details

--- app/in-memory-data.service.ts
+++ app/in-memory-data.service.ts
@@ -5,19 +5,19 @@ export class InMemoryDataService {
             date: '8/24/2016, 10:00:00 AM',
             name: 'How to become stripper fairies',
             image: '../images/fairy.jpg',
-            text: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer iaculis, risus at convallis pellentesque, ipsum est auctor massa, ac malesuada enim magna quis dolor. Ut nisl velit, tempus vitae ex non, vehicula rutrum dui. Phasellus viverra hendrerit finibus. Etiam ante lacus, eleifend sed felis sed, finibus blandit felis. Nullam molestie turpis nec tempor interdum. Donec fringilla nisi ut scelerisque auctor. Cras non nunc ut dolor finibus gravida at eget ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam in quam est. Nam aliquet vitae enim id efficitur. Etiam ornare velit enim, sed accumsan diam placerat a. Duis tincidunt neque vel ornare efficitur.</p><p>Morbi accumsan tincidunt velit, non lobortis lorem. Fusce sodales orci at feugiat elementum. Vivamus posuere iaculis nunc sit amet vehicula. Duis in lacinia dolor. Proin semper tristique diam, eget congue eros malesuada vitae. Sed mollis quam ut sem vulputate, ac convallis eros vehicula. Phasellus et lectus ac eros vehicula blandit. Proin ac lobortis diam. Duis leo erat, ornare eu auctor vel, tristique at ex. Fusce et leo mollis, fermentum nisi non, vehicula lectus. Sed nunc leo, vulputate nec enim molestie, sollicitudin aliquet augue. Duis vel nisi felis. Nunc quis felis eu libero venenatis mattis. Cras libero urna, bibendum vitae elementum id, faucibus et metus. Nunc venenatis arcu in consectetur dignissim. In hac habitasse platea dictumst.</p>'
+            text: '<p>How does Red Naga Training lead to a pilgramige to the grand wooden tree penis? Come and find out. Here we will cover the ancient technqiues first revealed in a Mid Summer Night's dream. How to lure stripper fairies with magical flutes, and advanced fart noises will be covered.</p>'
         }, {
             id: 2,
             date: '8/24/2016, 12:00:00 PM',
             name: 'How to get thrown out of a wine tour train',
             image: '',
-            text: ''
+            text: '<p>Are you a fan of the Lone Ranger? Ever wonder how much grape juice can be consumed by a lone man. Well look no further. In this talk Jcase demostrates proper esculation techniques for jerks on trains, how to juggle wine barrels, and how to tuck and roll at high speeds. Listeners are encouraged to bring their own wine openers and glasses</p>.'
         }, {
             id: 3,
             date: '8/24/2016, 02:00:00 PM',
             name: 'Passing out on the steps 101',
             image: '',
-            text: ''
+            text: '<p>Ever wonder what a reverser would see on a 'spirit' walk? Come and find out. Ben will recount the proper way to play kickball, drink, get free shots from army recruits, puke out of uber vehicles, scale Mount Everast stairs, propper concrete spooning techniques, toilet bowel cleaning, and bug bite medicine application techniques. The talk with conclude with bed bug sniffing dog recomenations</p>'
         }, {
             id: 4,
             date: '8/24/2016, 04:00:00 PM',
@@ -28,19 +28,19 @@ export class InMemoryDataService {
             date: '8/25/2016, 10:00:00 AM',
             name: 'Naked crack addict and bootloader unlocks',
             image: '',
-            text: ''
+            text: '<p>This talk will cover how to summon the rooting gods by performing the crack addict, boot loader dance.</p> '
         }, {
             id: 6,
             date: '8/25/2016, 12:00:00 PM',
             name: 'RubberMan',
             image: '',
-            text: ''
+            text: '<p>To sneak condoms into Diff's easter basket. This talk will go through, in low level detail, how to arrange easter baskets, put condoms in eggs, and how to arrange candy to fool beetbop</p>'
         }, {
             id: 7,
             date: '8/25/2016, 02:00:00 PM',
             name: 'Fuzzing with bees.',
             image: '',
-            text: ''
+            text: '<p>Learn how to make your bees work for you and not againest you. By propperly mixing honey, pollen, viagra, and chilli pepper, bees can be used with American Fuzzy Lop to find new and intereting never before seen bugs.</p>'
         }, ];
         return {events};
     }
