04b70f65b2a2b77f9904c55bf227fa6d03ca4b18
Becky Siegel <rsiegel@groupon.com>
2016-08-16 12:11:51 -0700

    escaping apostraphes

--- app/in-memory-data.service.ts
+++ app/in-memory-data.service.ts
@@ -5,19 +5,19 @@ export class InMemoryDataService {
             date: '8/24/2016, 10:00:00 AM',
             name: 'How to become stripper fairies',
             image: '../images/fairy.jpg',
-            text: '<p>How does Red Naga Training lead to a pilgramige to the grand wooden tree penis? Come and find out. Here we will cover the ancient technqiues first revealed in a Mid Summer Night's dream. How to lure stripper fairies with magical flutes, and advanced fart noises will be covered.</p>'
+            text: '<p>How does Red Naga Training lead to a pilgramige to the grand wooden tree penis? Come and find out. Here we will cover the ancient technqiues first revealed in a Mid Summer Night\'s dream. How to lure stripper fairies with magical flutes, and advanced fart noises will be covered.</p>'
         }, {
             id: 2,
             date: '8/24/2016, 12:00:00 PM',
             name: 'How to get thrown out of a wine tour train',
             image: '',
-            text: '<p>Are you a fan of the Lone Ranger? Ever wonder how much grape juice can be consumed by a lone man. Well look no further. In this talk Jcase demostrates proper esculation techniques for jerks on trains, how to juggle wine barrels, and how to tuck and roll at high speeds. Listeners are encouraged to bring their own wine openers and glasses</p>.'
+            text: '<p>Are you a fan of the Lone Ranger? Ever wonder how much grape juice can be consumed by a lone man. Well look no further. In this talk Jcase demostrates proper esculation techniques for jerks on trains, how to juggle wine barrels, and how to tuck and roll at high speeds. Listeners are encouraged to bring their own wine openers and glasses</p>'
         }, {
             id: 3,
             date: '8/24/2016, 02:00:00 PM',
             name: 'Passing out on the steps 101',
             image: '',
-            text: '<p>Ever wonder what a reverser would see on a 'spirit' walk? Come and find out. Ben will recount the proper way to play kickball, drink, get free shots from army recruits, puke out of uber vehicles, scale Mount Everast stairs, propper concrete spooning techniques, toilet bowel cleaning, and bug bite medicine application techniques. The talk with conclude with bed bug sniffing dog recomenations</p>'
+            text: '<p>Ever wonder what a reverser would see on a \'spirit\' walk? Come and find out. Ben will recount the proper way to play kickball, drink, get free shots from army recruits, puke out of uber vehicles, scale Mount Everast stairs, propper concrete spooning techniques, toilet bowel cleaning, and bug bite medicine application techniques. The talk with conclude with bed bug sniffing dog recomenations</p>'
         }, {
             id: 4,
             date: '8/24/2016, 04:00:00 PM',
@@ -28,13 +28,13 @@ export class InMemoryDataService {
             date: '8/25/2016, 10:00:00 AM',
             name: 'Naked crack addict and bootloader unlocks',
             image: '',
-            text: '<p>This talk will cover how to summon the rooting gods by performing the crack addict, boot loader dance.</p> '
+            text: '<p>This talk will cover how to summon the rooting gods by performing the crack addict, boot loader dance.</p>'
         }, {
             id: 6,
             date: '8/25/2016, 12:00:00 PM',
             name: 'RubberMan',
             image: '',
-            text: '<p>To sneak condoms into Diff's easter basket. This talk will go through, in low level detail, how to arrange easter baskets, put condoms in eggs, and how to arrange candy to fool beetbop</p>'
+            text: '<p>To sneak condoms into Diff\'s easter basket. This talk will go through, in low level detail, how to arrange easter baskets, put condoms in eggs, and how to arrange candy to fool beetbop</p>'
         }, {
             id: 7,
             date: '8/25/2016, 02:00:00 PM',
