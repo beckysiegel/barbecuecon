370108b1a459f8dab4fa90b8758a8a8355eca123
Ben Actis <bactis@synack.com>
2016-08-16 12:23:22 -0700

    added gifs

--- app/in-memory-data.service.ts
+++ app/in-memory-data.service.ts
@@ -10,36 +10,36 @@ export class InMemoryDataService {
             id: 2,
             date: '8/24/2016, 12:00:00 PM',
             name: 'How to get thrown out of a wine tour train',
-            image: '',
+            image: '../images/train.gif',
             text: '<p>Are you a fan of the Lone Ranger? Ever wonder how much grape juice can be consumed by a lone man. Well look no further. In this talk Jcase demostrates proper esculation techniques for jerks on trains, how to juggle wine barrels, and how to tuck and roll at high speeds. Listeners are encouraged to bring their own wine openers and glasses</p>.'
         }, {
             id: 3,
             date: '8/24/2016, 02:00:00 PM',
             name: 'Passing out on the steps 101',
-            image: '',
+            image: '../images/sleeping.gif',
             text: '<p>Ever wonder what a reverser would see on a 'spirit' walk? Come and find out. Ben will recount the proper way to play kickball, drink, get free shots from army recruits, puke out of uber vehicles, scale Mount Everast stairs, propper concrete spooning techniques, toilet bowel cleaning, and bug bite medicine application techniques. The talk with conclude with bed bug sniffing dog recomenations</p>'
         }, {
             id: 4,
             date: '8/24/2016, 04:00:00 PM',
             name: 'Rooting while hungover',
-            image: ''
+            image: '../images/hungover.gif'
         }, {
             id: 5,
             date: '8/25/2016, 10:00:00 AM',
             name: 'Naked crack addict and bootloader unlocks',
-            image: '',
+            image: '../images/dance.gif',
             text: '<p>This talk will cover how to summon the rooting gods by performing the crack addict, boot loader dance.</p> '
         }, {
             id: 6,
             date: '8/25/2016, 12:00:00 PM',
             name: 'RubberMan',
-            image: '',
+            image: '../images/easter.gif',
             text: '<p>To sneak condoms into Diff's easter basket. This talk will go through, in low level detail, how to arrange easter baskets, put condoms in eggs, and how to arrange candy to fool beetbop</p>'
         }, {
             id: 7,
             date: '8/25/2016, 02:00:00 PM',
             name: 'Fuzzing with bees.',
-            image: '',
+            image: '../images/bees.gif',
             text: '<p>Learn how to make your bees work for you and not againest you. By propperly mixing honey, pollen, viagra, and chilli pepper, bees can be used with American Fuzzy Lop to find new and intereting never before seen bugs.</p>'
         }, ];
         return {events};
